a202b0fae36adb2fb38769eea61a69acf1b06a31
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-24 13:36:56 -0400

    Update heading and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Finance Calculator</h1>
+<p>Calculate how much you are spending on everyday items.</p>
