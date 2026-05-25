59fbbc4b89e5917d656733c871138a946b6a81c5
Oleg Polin <140200108+olegpolin@users.noreply.github.com>
2026-05-25 14:27:06 -0400

    Add aria label to GitHub link for accessibility

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -14,6 +14,7 @@
     href="https://github.com/olegpolin/finance-calculator"
     target="_blank"
     rel="noopener noreferrer"
+    aria-label="GitHub repository"
   >
     <svg class="fill-foreground" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96">
       <path
